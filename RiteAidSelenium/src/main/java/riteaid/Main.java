9764380f7cc643eb8065b40349afb73b070cd3c5
fewasd <randomhumanbeing12345@gmail.com>
2022-11-20 14:06:02 -0800

    Removed unnecesary boiler code

--- RiteAidSelenium/src/main/java/riteaid/Main.java
+++ RiteAidSelenium/src/main/java/riteaid/Main.java
@@ -1,7 +1,24 @@
 package riteaid;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+        driver.get("https://www.riteaid.com/signup");
+        String title = driver.getTitle();
+
+        assert title.contains("Signup");
+
+        driver.quit();
+        System.out.println("Done!");
     }
 }
